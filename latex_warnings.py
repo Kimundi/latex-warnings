5057824e2b3fcc9046f9cc7421c00987ffd1668c
Marvin Löbel <loebel.marvin@gmail.com>
2019-03-21 11:27:02 +0100

    more options and controll

--- latex_warnings.py
+++ latex_warnings.py
@@ -6,6 +6,7 @@ import sys
 import os
 import subprocess
 import re
+import argparse
 
 CEND      = '\33[0m'
 CBOLD     = '\33[1m'
@@ -57,7 +58,8 @@ def colorize(text, colorcode):
     else:
         return str(text)
 
-re_warning = re.compile("^!|Warning|Error")
+re_error = re.compile("^!|Error")
+re_warning = re.compile("Warning")
 re_full = re.compile("Overfull|Underfull")
 re_path = re.compile("(\\./.*?\\.(pygtex|pygstyle|tex|pdf|png|toc|sty|w18))")
 re_run = re.compile("Run number [0-9]+ of rule")
@@ -65,18 +67,52 @@ re_run = re.compile("Run number [0-9]+ of rule")
 todo_words = ["TODO", "FIXME", "todo", "fixme", "Todo", "Fixme"]
 re_todo = re.compile("|".join(todo_words))
 
-cmd = sys.argv[1:]
 
-verbose = False
-if len(cmd) > 0 and cmd[0] == "-V":
-    verbose = True
-    cmd = cmd[1:]
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('-w', '--warnings', action='store_true',
+                    help='output warnings')
+parser.add_argument('-e', '--errors', action='store_true',
+                    help='output errors')
 
-print_all_files = verbose
-print_full_boxes = verbose
+parser.add_argument('-b', '--warn-box', action='store_true',
+                    help='output overfull and underfull box warnings')
+parser.add_argument('-t', '--warn-todo', action='store_true',
+                    help='output todo warnings')
+parser.add_argument('-f', '--all-files', action='store_true',
+                    help='print all processed file paths, not just *.tex')
 
+parser.add_argument('-n', '--no-raw', action='store_true',
+                    help='do not output the raw stdout and stderr of the wrapped process')
+
+parser.add_argument('-a', '--all', action='store_true',
+                    help='enables -webt')
+parser.add_argument('-V', '--verbose', action='store_true',
+                    help='enables -webtf')
+
+parser.add_argument('latex_commandline',
+                    help='the latex commandline to wrap')
+#parser.add_argument('command_args', nargs='*',
+#                    help='arguments for the latex command')
+
+(args, extra) = parser.parse_known_args()
+
+# Optional warnings
+print_full_boxes = args.warn_box  or args.all or args.verbose
+print_todo       = args.warn_todo or args.all or args.verbose
+print_warnings   = args.warnings  or args.all or args.verbose
+print_errors     = args.errors    or args.all or args.verbose
+
+# Optional warnings, verbose
+print_all_files  = args.all_files             or args.verbose
+
+# More complicated features
+print_no_raw = args.no_raw
+
+# Chained command
+cmd = [args.latex_commandline] + extra
 #print("Wrapper script executes: {}".format(cmd))
 
+# Ensure latex command does not cause early line breaks
 env = os.environ.copy()
 env["max_print_line"] = "10000"
 
@@ -89,7 +125,8 @@ output = []
 for line in iter(process.stdout.readline, b''):
     clean_line = line.decode("utf-8", "backslashreplace")
     output.append(clean_line)
-    print(clean_line.strip()),
+    if not print_no_raw:
+        print(clean_line.strip()),
 
 process.stdout.close()
 returncode = process.wait()
@@ -120,7 +157,7 @@ for line in output:
             print("File {}".format(colorize(last_file, CGREEN)))
         if last_file_candidate.endswith(".tex"):
             last_file = last_file_candidate
-            if os.path.isfile(last_file):
+            if print_todo and os.path.isfile(last_file):
                 with open(last_file, 'r') as f:
                     for i,line2 in enumerate(f.readlines()):
                         if re_todo.search(line2):
@@ -128,15 +165,21 @@ for line in output:
                                 line2 = line2.replace(todo_word, colorize(todo_word, CVIOLET))
                             print_warning("{} on line {}: {}".format(colorize("Todo", CVIOLET), i, line2))
 
-    if re_warning.search(line):
-        line = line.replace("Warning", colorize("Warning", CYELLOW))
+    found = False
+    if print_errors and re_error.search(line):
         line = line.replace("Error", colorize("Error", CRED))
         if line.startswith("!"):
             line = colorize("Error", CRED) + ": " + line
-        print_warning(line)
+        found = True
+    if print_warnings and re_warning.search(line):
+        line = line.replace("Warning", colorize("Warning", CYELLOW))
+        found = True
     if print_full_boxes and re_full.search(line):
         line = line.replace("Overfull", colorize("Overfull", CBLUE))
         line = line.replace("Underfull", colorize("Underfull", CBLUE))
+        found = True
+
+    if found:
         print_warning(line)
 
 exit(returncode)
